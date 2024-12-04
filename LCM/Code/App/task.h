42d485519545c4b08602438dab64fa19232f2f3e
Gene Kwak <rsprideio@gmail.com>
2024-12-04 13:00:20 -0800

    Increase SHUTDOWN_TIME from 10 to 20 min

--- LCM/Code/App/task.h
+++ LCM/Code/App/task.h
@@ -35,12 +35,12 @@
 #define   DEFAULT_CELL_TYPE			P42A
 #endif
 
-#define   SHUTDOWN_TIME				10
-#define   VESC_RPM					1000
-#define   VESC_BOOT_TIME			4000
-#define   VESC_SHUTDOWN_TIME		1000
-#define   DUTY_CYCLE				0.9
-#define   VOLTAGE_RECEIPT			0.02
+#define   SHUTDOWN_TIME		  		20
+#define   VESC_RPM            		1000
+#define   VESC_BOOT_TIME      		4000
+#define   VESC_SHUTDOWN_TIME      	1000
+#define   DUTY_CYCLE          		0.9
+#define   VOLTAGE_RECEIPT     		0.02
 #define	  CHARGE_COMMAND_TIME		1000 		// frequency of notifying the float package of current charge state
 /*******************************************************************************/
 #define   VESC_RPM_WIDTH			-200
